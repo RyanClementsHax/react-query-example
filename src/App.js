2175984c61237b045094aa94625bfc303d88dd57
Ryan Clements <ryanclementshax@gmail.com>
2021-09-09 09:20:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
+
+function App() {
+  let [page, setPage] = useState("planets");
+
+  return (
+    <div className="App">
+      <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        {page === "planets" ? <Planets /> : <People />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
