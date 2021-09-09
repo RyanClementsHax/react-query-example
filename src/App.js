5a09ab72a23a3731ff09ce66507077aa1ca3a206
Ryan Clements <ryanclementshax@gmail.com>
2021-09-09 10:27:09 -0400

    use pagination

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   let [page, setPage] = useState("planets");
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+    <>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
